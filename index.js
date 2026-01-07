28b1d3c65a28797828c25ffa3d88607db0434c27
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-01-07 18:01:19 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js файл для Media Station X на ТВ
+
+(function() {
+    var config = {
+        name: "Мой ТВ Плейлист",
+        version: "2026.01.07",
+        // Ссылка на ваш M3U файл на GitHub Pages
+        // Обратите внимание на полный HTTPS адрес!
+        content:
+    };
+
+    if (typeof msxApp !== 'undefined') {
+        msxApp.init(config);
+    } else {
+        console.error("Плеер не обнаружен.");
+    }
+})();
