78d0b1862d8dfe9be1f8a9d651c261ac525a6de2
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-01-08 11:54:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 (function() {
     var config = {
         name: "Мой ТВ Плейлист",
-        version: "1.0.0",
-        // Параметр "content" должен указывать на тип контента или список элементов
-        content: "https://raw.githubusercontent.com/Dmitriy11223455/tv/refs/heads/main/playlist_928374hfkj.m3u"
+        version: "1.0.1",
+        // Ссылка на ваш M3U файл в этом же репозитории
+        content: "raw.githubusercontent.com"
     };
 
-    // Проверка окружения MSX
     if (typeof TVXAdminTools !== 'undefined') {
         TVXAdminTools.initApp(config);
     } else if (window.msxApp) {
         msxApp.init(config);
-    } else {
-        console.log("Конфигурация готова: ", config);
     }
 })();
