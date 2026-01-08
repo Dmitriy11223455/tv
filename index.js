068751685305fb3f102163623374f66931bc0964
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-01-08 11:35:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// index.js файл для Media Station X на ТВ
-
 (function() {
     var config = {
         name: "Мой ТВ Плейлист",
-        version: "2026.01.07",
-        
-        https://raw.githubusercontent.com/Dmitriy11223455/tv/refs/heads/main/playlist_928374hfkj.m3u
-        content:
+        version: "1.0.0",
+        // Параметр "content" должен указывать на тип контента или список элементов
+        content: "https://raw.githubusercontent.com/Dmitriy11223455/tv/refs/heads/main/playlist_928374hfkj.m3u"
     };
 
-    if (typeof msxApp !== 'undefined') {
+    // Проверка окружения MSX
+    if (typeof TVXAdminTools !== 'undefined') {
+        TVXAdminTools.initApp(config);
+    } else if (window.msxApp) {
         msxApp.init(config);
     } else {
-        console.error("Плеер не обнаружен.");
+        console.log("Конфигурация готова: ", config);
     }
 })();
