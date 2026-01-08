6b56e398e33d45161b92f3fee8be3391eebd051a
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-01-08 11:55:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
         name: "Мой ТВ Плейлист",
         version: "1.0.1",
         // Ссылка на ваш M3U файл в этом же репозитории
-        content: "raw.githubusercontent.com"
+        content: "https://raw.githubusercontent.com/Dmitriy11223455/tv/refs/heads/main/playlist_928374hfkj.m3u"
     };
 
     if (typeof TVXAdminTools !== 'undefined') {
