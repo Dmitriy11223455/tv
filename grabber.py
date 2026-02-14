50c7a971eb48ea81e02d0e63b0dfb2b13c6bb2f5
Dmitriy11223455 <vbarishnik50@gmail.com>
2026-02-14 17:52:20 +0200

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -42,8 +42,7 @@ async def get_all_channels_from_site(page):
 async def get_tokens_and_make_playlist():
     # ТВОЙ СЛОВАРЬ (Приоритетные каналы)
     MY_CHANNELS = {
-        "РОССИЯ 1": "https://smotrettv.com/784-rossija-1.html",
-        "НТВ": "https://smotrettv.com/6-ntv.html"
+        "РОССИЯ 1": "https://smotrettv.com/784-rossija-1.html"
     }
 
     async with async_playwright() as p:
